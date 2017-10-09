1de4cd854c1a0b1eb627159fa966f65c0cc9788a
amresh087 <geniousamresh@gmail.com>
2017-10-09 22:04:48 +0530

    add classes

---
+++ OAuth2ResourceOwnerUser/src/main/java/com/amresh/oauth2/OAuth2ResourceOwnerUser/ResOwnerApp.java
@@ -0,0 +1,20 @@
+package com.amresh.oauth2.OAuth2ResourceOwnerUser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class ResOwnerApp extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(ResOwnerApp.class, args);
+    }
+    
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ResOwnerApp.class);
+	}
+}
