260beb60945136de71825f46076bf7e66256d7d3
amresh087 <geniousamresh@gmail.com>
2017-10-09 21:57:33 +0530

    add class

--- OAuth2ResourceServer/src/main/java/com/amresh/oauth2/OAuth2ResourceServer/OAuth2ResourceServerApplication.java
+++ OAuth2ResourceServer/src/main/java/com/amresh/oauth2/OAuth2ResourceServer/OAuth2ResourceServerApplication.java
@@ -1,12 +1,25 @@
 package com.amresh.oauth2.OAuth2ResourceServer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+
 
 @SpringBootApplication
-public class OAuth2ResourceServerApplication {
+@EnableAutoConfiguration
+@ComponentScan(basePackages="com.amresh.oauth2")
+public class OAuth2ResourceServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OAuth2ResourceServerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(OAuth2ResourceServerApplication.class);
 	}
 }
